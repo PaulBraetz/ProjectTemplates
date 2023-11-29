a08cd6fd10e8f0160a2aa0380a5cbaf1beb1a38c
Paul Braetz <paul.braetz@rhomicro.com>
2023-11-29 21:21:29 +0100

    add console, generator templates

---
+++ Generator/Generator.cs
@@ -0,0 +1,12 @@
+namespace RhoMicro.CodeAnalysis;
+
+using Microsoft.CodeAnalysis;
+
+[Generator(LanguageNames.CSharp)]
+public sealed class $(ProjectName) : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        
+    }
+}
